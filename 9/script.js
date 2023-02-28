1524538320184578966256ef06f2dd5acb86b824
shawon012 <shawon.shakib01@gmail.com>
2023-02-28 12:34:24 +0600

    9 no added

---
+++ 9/script.js
@@ -0,0 +1,59 @@
+// javascript tricky concept
+// primitive reference type
+// non primitive type
+// falsy vs truthy
+// null vs undefined
+// == vs ===
+// scope
+// scope errors
+// hoisting
+// more about hoisting
+// hoisting in function
+// hoisiting and scope
+// vat vs let vs const
+// closure
+// call back function
+// pass by reference vs pass by value
+
+// practice problem 1
+// 1. if i write [] == true, will show true of false? check it out.
+// 2. use === to check if the true === 'true' gives true of false.
+// 3. write an arrow function that will take a parameter and will check if the parametet is a number or not by using isNaN(). And return true of false.
+
+// practice problem 2
+// change the value in the storefalsyValue variable to a falsy value, such that the code in the else statement executes
+
+
+// let storefalseValue = true;
+
+// if(storefalseValue){
+//     console.log('Nothing to show');
+// }
+// else{
+//     console.log('You are amazing');
+// }
+
+
+// practice problem 3
+// write an arrow function taht will take 3 parameters (first two parameters will be numbers and the third parameter will be a string) that will perform arithmetic operation depending on the third parameter and will print the result. for example:
+
+// 1. Print result of num1 + num2 if operation is 'add'
+// 2. Print result of num1 - num2 if operation is 'subtract'
+// 3. Print result of num1*num2 if operation is 'multiply'
+// 4. Print result of num1/num2 if operation is 'divide'
+// 5. Print result of num1%num2 if operation is 'modulus'
+// 6. Else print 'Invalid operation'
+
+
+
+// common interview questions
+// 1. tell the difference between primitive and non primitive data types in javascript.
+// 2. What are the Truthy and values? Give me some examples.
+// 3. What is the difference between null and undefined?
+// 4. What are the difference between double equal (==) and triple equal (===)?
+// 5. What is scope in Javascript?
+// 6. Define block scope and global scope.
+// 7. What is hoisting in Javascript?
+// 8. How to use the Javascript callback function?
+// 9. Explain closure in Javascript
+// 10. Explain passed by value and passed by reference.
